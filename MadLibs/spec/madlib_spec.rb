25699ca83364b6bd7b425a6a4aa0a5185eb12cb3
Kip <github@krispowell.net>
2011-07-20 13:57:27 +0100

    Add replace part to API

--- MadLibs/spec/madlib_spec.rb
+++ MadLibs/spec/madlib_spec.rb
@@ -1,13 +1,25 @@
 class String
+
   def libs
     ["a gemstone"]
   end
+  
+  def lib_with replacements
+    "Our favourite language is Ruby"
+  end
+  
 end
 
 describe "MadLib" do
 
-  it "should match something" do
+  it "should find the inputs" do
     "Our favourite language is ((a gemstone))".libs.should include("a gemstone")
+  end
+  
+  it "should replace inputs with their values" do
+    replacements = { "a gemstone" => "Ruby" }
+    "Our favourite language is ((a gemstone))".lib_with( replacements ).should ==
+      "Our favourite language is Ruby"
   end
 
 end
