dd0eb15d3abf6f8e42c3ccb7e81018129690365a
Kip <github@krispowell.net>
2011-07-20 14:39:48 +0100

    oops, two things: it is done and it handled multi replacements

--- MadLibs/spec/madlib_spec.rb
+++ MadLibs/spec/madlib_spec.rb
@@ -1,24 +1,44 @@
-class String
+require 'src/madlib'
 
-  def libs
-    self.scan(/(\(\([^)]+\)\))/).flatten
+class MockSource
+
+  def initialize( results )
+    @results = results
+  end
+
+  def puts input
+    @looking_for = input
   end
   
-  def lib_with replacements
-    self.sub( /\(\(([^)]+)\)\)/ , replacements )
+  def gets
+    @results[ @looking_for ]
   end
   
 end
 
 describe "MadLib" do
 
+  def replacements
+    { "((a gemstone))" => "Ruby", "((a letter))" => "K" }
+  end
+
   it "should find the madlib tokens" do
     "Our favourite language is ((a gemstone))".libs.should include("((a gemstone))")
   end
   
   it "should replace madlib tokens with provided values" do
-    replacements = { "((a gemstone))" => "Ruby" }
     "Our favourite language is ((a gemstone))".lib_with( replacements ).should ==
+      "Our favourite language is Ruby"
+  end
+
+  it "should replace multiple madlib tokens with provided values" do
+    "Our favourite language is ((a gemstone)), much better than ((a letter))".lib_with( replacements ).should ==
+      "Our favourite language is Ruby, much better than K"
+  end
+  
+  it "should prompt for found tokens and return the results" do
+    source = MockSource.new( replacements )
+    "Our favourite language is ((a gemstone))".madlib_it( source, source ).should == 
       "Our favourite language is Ruby"
   end
 
