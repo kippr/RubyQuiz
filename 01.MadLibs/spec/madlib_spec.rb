bb23f72c8d21cc8257ae4e00fe5bdca33a52ad2b
Kip <github@krispowell.net>
2011-08-13 17:45:42 +0100

    Fix path issues for other quizzes

--- 01.MadLibs/spec/madlib_spec.rb
+++ 01.MadLibs/spec/madlib_spec.rb
@@ -1,4 +1,4 @@
-require 'src/madlib'
+require File.join(File.expand_path(File.dirname(__FILE__)), "..", "src", "madlib")
 
 class MockSource
 
--- 02.LCD/spec/lcd_spec.rb
+++ 02.LCD/spec/lcd_spec.rb
@@ -1,4 +1,4 @@
-require 'src/lcd.rb'
+require File.join(File.expand_path(File.dirname(__FILE__)), "..", "src", "lcd")
 
 describe LcdWriter do
   
