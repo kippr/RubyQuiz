dda11e8b74eed1bc311e2cf984578b9700daf174
Kip <github@krispowell.net>
2011-08-14 18:40:16 +0100

    Add a y/n branch for first question

--- 04.AnimalQuiz/spec/animal_spec.rb
+++ 04.AnimalQuiz/spec/animal_spec.rb
@@ -3,18 +3,26 @@ describe 'AnimalQuiz' do
 
   before :each do
     @questions = [] << "Think of an animal..." << "Is it an elephant? (y/n)"
-    @questions << 'I win! Pretty smart! Play again? (y/n)'
   end
 
   it 'should start simple' do
-    assert_first_question
+    ask_is_it_an_elephant
   end
   
   it 'should be happy if you get lucky' do
-    assert_first_question
+    ask_is_it_an_elephant
     answer 'y'
     question.should == 'I win! Pretty smart! Play again? (y/n)'
   end
+
+  it 'should ask for a clue if it didnt win' do
+    ask_is_it_an_elephant
+    answer 'n'
+    question.should == 'You win, well done! Before you go, help me learn...'
+    question.should == 'What animal were you thinking of?'
+  end
+
+  
 
   it "should" do
     pending
@@ -28,9 +36,11 @@ describe 'AnimalQuiz' do
   end
   
   def answer reply
+    @questions << 'I win! Pretty smart! Play again? (y/n)' if reply == 'y'
+    @questions << 'You win, well done! Before you go, help me learn...' << 'What animal were you thinking of?' if reply == 'n'
   end
   
-  def assert_first_question
+  def ask_is_it_an_elephant
     question.should == 'Think of an animal...'
     question.should == 'Is it an elephant? (y/n)'
   end
